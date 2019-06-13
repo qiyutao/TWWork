2c26fc742c4d326228dd3d7d925b66efd005653c
DaiQing <dq_199510@163.com>
2019-06-13 08:53:50 +0800

    add terminal version

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,13 +1,46 @@
 package start;
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
+import java.io.IOException;
+
+import algo.GameMatrix;
 import view.AlgoFrame;
 
 public class Main {
+
+	private static boolean controller = false;
+
+	public static void PrintMatrix(boolean[][] matrix) {
+		for (int i = 0; i < matrix.length; ++i) {
+			for (int j = 0; j < matrix[0].length; ++j) {
+				System.out.print(matrix[i][j] ? "*" : "-");
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
-		//1. Java事件分发线程
-		EventQueue.invokeLater(() -> {
-			AlgoFrame frame = new AlgoFrame("Game of life");
-		});
+		if (controller) {
+			GameMatrix gameMatrix = new GameMatrix(40, 30, 4);
+			gameMatrix.init();
+			while (true) {
+				try {
+					gameMatrix.nextMatrix();
+					Main.PrintMatrix(gameMatrix.getMatrix());
+					Thread.sleep(400);
+					Runtime.getRuntime().exec("clear");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			try {
+				// 1. Java事件分发线程
+				EventQueue.invokeLater(() -> {
+					AlgoFrame frame = new AlgoFrame("Game of life");
+				});
+			} catch (Exception e) {
+			}
+		}
 	}
 }
